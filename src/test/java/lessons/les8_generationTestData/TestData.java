0a91b50d0160f76ec54a1b527e48696e41929730
Aleksei Maksimov <maximov0603@gmail.com>
2024-02-06 20:39:24 +0300

    added lesson_8 static test data

---
+++ src/test/java/lessons/les8_generationTestData/TestData.java
@@ -0,0 +1,10 @@
+package lessons.les8_generationTestData;
+
+public class TestData { //можно испортировать этот класс в нужно тесте
+        public static String firstName = "Alex",
+                lastName = "Maximov",
+                email = "maximov@mail.ru",
+                gender = "Male",
+                userNumber = "1111111111";
+}
+
